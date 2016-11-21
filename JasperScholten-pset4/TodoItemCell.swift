8217f30eee83233db4d4fce7575ba6c96ee50bc4
jasperscholten <j.o.scholten@student.vu.nl>
2016-11-21 14:30:26 +0100

    Made a basic interface, with a tableView and input field with add button.

---
+++ JasperScholten-pset4/TodoItemCell.swift
@@ -0,0 +1,26 @@
+ //
+//  TodoItemCell.swift
+//  JasperScholten-pset4
+//
+//  Created by Jasper Scholten on 20-11-16.
+//  Copyright © 2016 Jasper Scholten. All rights reserved.
+//
+
+import UIKit
+
+class TodoItemCell: UITableViewCell {
+
+    @IBOutlet weak var listItem: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- JasperScholten-pset4/ViewController.swift
+++ JasperScholten-pset4/ViewController.swift
@@ -6,10 +6,20 @@
 //  Copyright © 2016 Jasper Scholten. All rights reserved.
 //
 
+// Add by clicking add button
+// Delete by swiping left - commitEditingStyle delegate function
+// Check (done) by swiping right
+// Rmember list between sessions
+
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var inputField: UITextField!
+    
+    var listItems = ["Type new item in field and click add", "Delete item by swiping left", "Check item by swiping right"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +30,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listItems.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "itemCell", for: indexPath) as! TodoItemCell
+        
+        cell.listItem.text = listItems[indexPath.row]
+        
+        return cell
+    
+    }
+    
+    @IBAction func addItem(_ sender: Any) {
+        
+        // add item to tableView
+        
+        // append to array
+        listItems.append(inputField.text!)
+        print(listItems)
+    }
 
 }
 
