24c0ab7e17a36195f0235b1e4823961f3f259b30
jasperscholten <j.o.scholten@student.vu.nl>
2016-11-21 18:12:33 +0100

    Added DatabaseHelper file. Commit because of recommended update.

---
+++ JasperScholten-pset4/DatabaseHelper.swift
@@ -0,0 +1,10 @@
+//
+//  DatabaseHelper.swift
+//  JasperScholten-pset4
+//
+//  Created by Jasper Scholten on 21-11-16.
+//  Copyright © 2016 Jasper Scholten. All rights reserved.
+//
+
+import Foundation
+import SQLite
